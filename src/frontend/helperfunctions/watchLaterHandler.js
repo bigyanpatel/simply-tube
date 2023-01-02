1ec181f326db53210629bf5184d993aa8d07d9ff
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2023-01-02 23:02:43 +0530

    Style: Utility class added

--- src/frontend/helperfunctions/watchLaterHandler.js
+++ src/frontend/helperfunctions/watchLaterHandler.js
@@ -64,7 +64,7 @@ export const deleteWatchLaterHandler = async (
       });
     }
   } catch (error) {
-    watchLaterDispatch({
+    dataStoreDispatch({
       type: actionTypes.API_ERROR,
       payload: "API is not working.",
     });
